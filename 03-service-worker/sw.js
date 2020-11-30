7f0cf74e2eb3c8226d723f97ad6ad82923e47d3a
Elio Reyes Silva <resiel@gmail.com>
2020-11-29 22:48:13 -0600

    Service Worker - Activate.

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -4,4 +4,12 @@ self.addEventListener('install', event => {
     // Descargar assets
     // Creamos un cache
     console.log('Installing Service Worker...');
+
+    self.skipWaiting();
+});
+
+// Cuando el Service Worker toma el control de la aplicación.
+self.addEventListener('activate', event => {
+    // Borrar cache viejo
+    console.log('Servicer Worker active and ready to control the app...');
 });
