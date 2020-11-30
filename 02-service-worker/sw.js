10cfda3ccaceba9de12ed84f78c1c385181784e4
Elio Reyes Silva <resiel@gmail.com>
2020-11-29 20:42:56 -0600

    Task - Intercept  and modify requests.

--- 02-service-worker/sw.js
+++ 02-service-worker/sw.js
@@ -1,15 +1,6 @@
 self.addEventListener('fetch', event => {
-    if (event.request.url.includes('style.css')) {
-        let response = new Response(`
-            body {
-                background-color: red !important;
-                color: pink;
-            }
-        `, {
-            headers: {
-                'Content-Type': 'text/css'
-            }
-        });
+    if (event.request.url.includes('main.jpg')) {
+        let response = fetch('img/main-patas-arriba.jpg');
 
         event.respondWith(response);
     }
